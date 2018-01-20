502af6a801446219ceca731ffca5495c98b40de0
Kupreeva Kristina <kd.kupreeva@physics.msu.ru>
2018-01-20 22:51:20 +0300

    Number Base

---
+++ elementary/number_base.py
@@ -0,0 +1,30 @@
+"""
+Do you remember the radix and Numeral systems from math class? Let's practice with it.
+You are given a positive number as a string along with the radix for it. Your function should convert it into decimal form. 
+The radix is less than 37 and greater than 1. The task uses digits and the letters A-Z for the strings.
+Watch out for cases when the number cannot be converted. For example: "1A" cannot be converted with radix 9. 
+For these cases your function should return -1.
+Input: Two arguments. A number as string and a radix as an integer.
+Output: The converted number as an integer.
+How it is used: Here you will learn how to work with the various numeral systems and handle exceptions.
+Precondition: 
+re.match("\A[A-Z0-9]\Z", str_number)
+0 < len(str_number) ≤ 10
+2 ≤ radix ≤ 36
+"""
+
+def checkio(str_number, radix):
+    try:
+        result = int(str_number, base = radix)
+    except ValueError:
+        result = -1
+    return result
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio("AF", 16) == 175, "Hex"
+    assert checkio("101", 2) == 5, "Bin"
+    assert checkio("101", 5) == 26, "5 base"
+    assert checkio("Z", 36) == 35, "Z base"
+    assert checkio("AB", 10) == -1, "B > A = 10"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
