590d6cb4113f2f3666f6f6c50d10ae1c401a2d61
Kupreeva Kristina <kd.kupreeva@physics.msu.ru>
2018-01-21 13:01:26 +0300

    House Password

---
+++ home/house_password.py
@@ -0,0 +1,34 @@
+"""
+Stephan and Sophia forget about security and use simple passwords for everything. Help Nikola develop a password security 
+check module. The password will be considered strong enough if its length is greater than or equal to 10 symbols, it has at 
+least one digit, as well as containing one uppercase letter and one lowercase letter in it. The password contains only ASCII 
+latin letters or digits.
+Input: A password as a string (Unicode for python 2.7).
+Output: Is the password safe or not as a boolean or any data type that can be converted and processed as a boolean. In the 
+results you will see the converted results.
+"""
+
+def checkio(data):
+
+    #replace this for solution
+    if len(data)<10:
+        return False
+    elif any(i.isdigit() for i in data) == False:
+        return False
+    elif any(i.isupper() for i in data) == False:
+        return False
+    elif any(i.islower() for i in data) == False:
+        return False
+    else:
+        return True
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
